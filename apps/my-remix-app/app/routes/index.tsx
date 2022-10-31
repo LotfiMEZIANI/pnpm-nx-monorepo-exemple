2cac4c775f6962589bfeb649c0f0ef438dc6b4c7
Lotfi MEZIANI <lotfi.meziani@epsor.fr>
2022-10-31 22:41:11 +0100

    chore: complete npm workspace init

---
+++ apps/my-remix-app/app/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "shared-ui"
+
+export default function Index() {
+    return (
+        <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.4"}}>
+            <h1>Welcome to My-Remix-App</h1>
+
+            <Button onClick={()=>console.log('clicked !')}>Click me</Button>
+        </div>
+    );
+}
---
+++ packages/shared-ui/Button.tsx
@@ -0,0 +1,3 @@
+export const Button = (props: any) => {
+    return <button onClick={() => props.onClick()}>{props.children}</button>;
+}
